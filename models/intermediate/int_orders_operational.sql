2d0a64936f151a8b1f95e48255fc4eebd8df9450
elieroukia@gmail.com <elieroukia@gmail.com>
2025-05-06 12:53:26 +0000

    update operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,13 +1,14 @@
-SELECT
-m.orders_id,
-m.date_date,
-ROUND(m.margin + sh.shipping_fee - sh.logcost - sh.ship_cost, 2) AS operational_margin,
-m.quantity,
-m.revenue,
-m.purchase_cost,
-sh.shipping_fee,
-sh.ship_cost,
-sh.logcost
-FROM {{ ref('int_orders_margin') }} m
-LEFT JOIN {{ ref('stg_raw__ship') }} sh
-ON m.orders_id = sh.orders_id
+select
+    m.orders_id,
+    m.date_date,
+    round(
+        m.margin + sh.shipping_fee - sh.logcost - sh.ship_cost, 2
+    ) as operational_margin,
+    m.quantity,
+    m.revenue,
+    m.purchase_cost,
+    sh.shipping_fee,
+    sh.ship_cost,
+    sh.logcost
+from {{ ref("int_orders_margin") }} m
+left join {{ ref("stg_raw__ship") }} sh on m.orders_id = sh.orders_id
