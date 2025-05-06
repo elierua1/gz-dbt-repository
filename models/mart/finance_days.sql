b0ec07f043a6ea3453a0c1c8c02e52285482213a
elieroukia@gmail.com <elieroukia@gmail.com>
2025-05-06 13:08:36 +0000

    finance days mart added

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,14 +1,14 @@
 select
 date_date,
 COUNT(orders_id) AS nb_transaction,
-SUM(revenue) AS revenue,
-SUM(revenue) / COUNT(orders_id) AS average_basket,
-SUM(margin) AS margin,
-SUM(operational_margin) AS operational_margin,
-SUM(purchase_cost) AS total_purchase_cost,
-SUM(shipping_fee) AS total_shipping_fees,
-SUM(logcost) AS total_log_costs,
-SUM(quantity) AS total_quantity_sold
+ROUND(SUM(revenue), 1) AS revenue,
+ROUND(SUM(revenue) / COUNT(orders_id), 1) AS average_basket,
+ROUND(SUM(margin), 1) AS margin,
+ROUND(SUM(operational_margin), 1) AS operational_margin,
+ROUND(SUM(purchase_cost), 1) AS total_purchase_cost,
+ROUND(SUM(shipping_fee), 1) AS total_shipping_fees,
+ROUND(SUM(logcost), 1) AS total_log_costs,
+ROUND(SUM(quantity), 1) AS total_quantity_sold
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
 ORDER BY date_date DESC
