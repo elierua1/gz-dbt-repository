88561447073a4187d225c342c82c1e8314b57563
elieroukia@gmail.com <elieroukia@gmail.com>
2025-05-07 09:32:51 +0000

    update on schema

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,7 @@
 select
 
-s.* ,
-p.purchase_price * s.quantity as purchase_cost,
-s.revenue - (p.purchase_price * s.quantity) as margin
-FROM {{ ref('stg_raw__sales') }} s
-LEFT JOIN {{ ref('stg_raw__product') }} p
-on s.product_id = p.products_id
+    s.*,
+    p.purchase_price * s.quantity as purchase_cost,
+    s.revenue - (p.purchase_price * s.quantity) as margin
+from {{ ref("stg_raw__sales") }} s
+left join {{ ref("stg_raw__product") }} p on s.product_id = p.products_id
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 select
 date_date,
 COUNT(orders_id) AS nb_transaction,
